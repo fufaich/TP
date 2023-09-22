2e8f1c706e3199c6283abdc518f78bc937a2774f
fufaich <fufaich@gmail.com>
2023-09-22 19:58:34 +0300

    Add Garage

---
+++ lab1/entity/Garage.h
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+class Garage{
+    private:
+        string brand; 
+        string model;
+    public:
+        Garage(string brand, string model):brand(brand), model(model){};
+        ~Garage(){};
+        
+        virtual string getBrand() = 0;
+        virtual string getModel() = 0;
+};
