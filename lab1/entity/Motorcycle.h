dcef34c9f7e5887fcdd4c4270f359f60bc83cd06
fufaich <fufaich@gmail.com>
2023-10-01 13:53:16 +0300

    Add class Motorcycle

--- lab1/entity/Motorcycle.h
+++ lab1/entity/Motorcycle.h
@@ -15,4 +15,8 @@ class Motorcycle: public Garage{
     public:
         Motorcycle(string brand, string model, float SizeEngine, int HP, string mission):Garage(brand, model),SizeEngine(SizeEngine),
         HP(HP), Mission(mission){};
+
+        float getSizeEngine() const;
+        int getHP() const;
+        string getMission() const;
 };
