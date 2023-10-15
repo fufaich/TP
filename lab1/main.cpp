5d8f4c6b6d926fa6e2b8870d383e07d657a76ad8
fufaich <fufaich@gmail.com>
2023-10-15 18:58:59 +0300

    Update

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -6,11 +6,13 @@
 
 int main(int *argc, char **argv){
     cout << "Hello wordl" << endl;
-    Keeper kep;
-    Auto BMW(string("BMW"), string("X5"), 3.5, string("Black"), string("Auto"));
-    Bus busik(string("Mercedes"), string("Busik"), 15,20,string("City"));
-    kep.addUnit(&BMW);
-    kep.addUnit(&busik);
-    kep.printToConsole();
+    Keeper* kep = new Keeper();
+    Auto* bmw= new Auto(string("BMW"), string("X5"), 3.5, string("Black"), string("Auto"));
+    Bus*  busik = new Bus(string("Mercedes"), string("Busik"), 15,20,string("City"));
+    kep->addUnit(bmw);
+    kep->addUnit(busik);
+    kep->printToConsole();
+
+    delete kep;
     return 0;
 }
