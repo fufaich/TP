626b5c4d969d537f4697acbb7344e35eaa86e8af
fufaich <fufaich@gmail.com>
2023-10-15 19:40:38 +0300

    Update

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -9,9 +9,13 @@ int main(int *argc, char **argv){
     Keeper* kep = new Keeper();
     Auto* bmw= new Auto(string("BMW"), string("X5"), 3.5, string("Black"), string("Auto"));
     Bus*  busik = new Bus(string("Mercedes"), string("Busik"), 15,20,string("City"));
+    Motorcycle* motik = new Motorcycle(string("Honda"), string("CBR600"), 4.5,205,string("Sport"));
     kep->addUnit(bmw);
     kep->addUnit(busik);
-    kep->printToConsole();
+    kep->addUnit(motik);
+    //kep->printToConsole();
+    //kep->loadToFile("test.txt");
+    kep->loadFromFile(string("test.txt"));
 
     delete kep;
     return 0;
